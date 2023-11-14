dba8e26a097574e13e7964e27f344046e6db6abf
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-11-14 13:37:24 +0400

    create server-driven-ui project

---
+++ server-driven-ui/server-driven-ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  server-driven-ui
+//
+//  Created by Василий Клецкин on 11/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
